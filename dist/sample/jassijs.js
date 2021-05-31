30fb24520f877813ccec74f74daa2aa934fd8c6a
uwei <udo_weigelt@web.de>
2021-06-01 00:58:56 +0200

    update dist

--- dist/sample/jassijs.js
+++ dist/sample/jassijs.js
@@ -63,7 +63,7 @@ async function run() {
         let allmodules = {};
         let dowait = [];
         for (let modul in modules) {
-            if (modules[modul].endsWith(".js")||modules[modul].indexOf(".js?")>-1) {
+            if (modules[modul].endsWith(".js") || modules[modul].indexOf(".js?") > -1) {
                 dowait.push(loadScript(modules[modul]));
             }
             /*let modulpath = "./" + modul + "/jassi.json";
@@ -99,7 +99,7 @@ async function run() {
                     let f = res[x].default.css[key];
                     if (f.indexOf(":") > -1) //https://cdn
                         cssFiles.push(f);
-                    else if (modpath.endsWith(".js")||modpath.indexOf(".js?")>-1) {
+                    else if (modpath.endsWith(".js") || modpath.indexOf(".js?") > -1) {
                         var m = modpath.substring(0, modpath.lastIndexOf("/"));
                         cssFiles.push(m + "/" + f);
                     }
@@ -129,20 +129,29 @@ async function run() {
             }
         }
         requirejs.config(requireconfig);
+        //read UserSettings after all beforestartlib are loaded
+        beforestartlib.push("jassi/jassi");
+        beforestartlib.push("jassi/remote/Settings");
         //load beforestartlib synchron
         async function loadBeforestart() {
             for (var x = 0; x < beforestartlib.length; x++) {
                 await new Promise((resolve) => {
                     require([beforestartlib[x]], function (beforestart) {
-                        if (beforestart?.autostart) {
+                        if (beforestart === null || beforestart === void 0 ? void 0 : beforestart.autostart) {
                             beforestart.autostart().then(() => {
                                 resolve(undefined);
-                            })
-                        } else {
+                            });
+                        }
+                        else {
                             resolve(undefined);
                         }
                     });
-                })
+                });
+                if (beforestartlib[x] === "jassi/jassi") {
+                    cssFiles.forEach((css) => {
+                        jassi.myRequire(css); //needed for Login Dialog
+                    });
+                }
             }
         }
         loadBeforestart().then(() => {
@@ -158,7 +167,6 @@ async function run() {
                 }
             });
         });
-
     });
 }
 ;
@@ -176,4 +184,5 @@ run();
     err.appendChild(node);
     return true;
 }*/
+//# sourceMappingURL=jassijs.js.map
 //# sourceMappingURL=jassijs.js.map
