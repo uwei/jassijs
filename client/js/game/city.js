1ffd05a0650e6eb3399b28077a5248e982956ac0
Udo Weigelt <udo.weigelt@web.de>
2023-02-03 13:56:54 +0100

    game

--- client/js/game/city.js
+++ client/js/game/city.js
@@ -537,7 +537,7 @@ define(["require", "exports", "game/citydialog", "game/company", "game/airplane"
         updateresetBuildingsWithoutCosts() {
             var _this = this;
             if (this.world.game.date.getHours() === 0) {
-                var test = getRandomInt(2000);
+                var test = getRandomInt(4000);
                 if (test === 0) {
                     this.domStar.style.display = "initial";
                     setTimeout(() => {
